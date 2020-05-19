51df504f612376539496355b6cbb17b842abc842
kratichordia <54771377+kratichordia@users.noreply.github.com>
2020-05-19 15:07:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-app.get('/',(req,res) =>{
-  res.send("Hello");
-})
+app.use(express.static(__dirname))
+
 app.listen(3000);
